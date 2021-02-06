74b5d7d68a901f8689dc223b8c30c0b6803ffe5a
npinghuang <huangenping0512@gapp.nthu.edu.tw>
2021-02-06 21:00:19 +0800

    failed to use C server

---
+++ src/goap_2021/src/mission_C_practice/getcup.h
@@ -0,0 +1,8 @@
+#ifndef GETCUP_H
+#define GETCUP_H
+#include <string>
+using namespace std;
+
+void getcup_action(int action[], int count, int length, int *ptr);
+
+#endif
--- src/goap_2021/src/mission_big/mission_big.py
+++ src/goap_2021/src/mission_big/mission_big.py
@@ -13,7 +13,7 @@ current = current_setting( 0, [0,0,0], 0, [0,0,0], 0 )
 # current.mission_pos[0] = input("mission pos x:")
 # current.mission_pos[1] = input("mission pos y:")
 # current.mission_pos[2] = input("mission pos theta:")
-# current.cup_no = input("mission cup no:")
+# current.cup_no = input("mission cup nob:")
 # current.planner = input("planner state 1 for at pos 0 for failed 2 for going:")
 # current.pos[0] = input("pos x:")
 # current.pos[1] = input("pos y:")
@@ -35,6 +35,8 @@ def trytry(current):
         print("yeah~")
 trytry(current)
 print("---------------------------------------------")
+
+
 def mission(req):
     print("req", req.action[0])
     state = 111
